10f9eb17741020a9db77c1feddbebde535fe516b
GabrielRodd <gabriel.rdgs33@outlook.com>
2026-05-13 11:18:43 -0300

    Primeiro commit, adiciona primeiros arquivos

---
+++ src/main/java/Arena.java
@@ -0,0 +1,18 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Arena {
+
+    private String nome;
+    private String endereco;
+    private String numero;
+
+}
+
+
+
+
